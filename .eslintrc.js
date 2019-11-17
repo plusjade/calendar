2ec90a85ac5f24fbcf6db6feffd33a2dabbe6108
Jade Dominguez <plusjade@gmail.com>
2019-11-17 08:46:56 -0800

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: [0],
+    'react/prop-types': [0],
+  },
+};
+
+
+
